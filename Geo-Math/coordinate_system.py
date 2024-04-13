36117e44c60b9631a91873f0409b6c46fe878b3f
Luis Schuimer <85784931+LuisSchuimer@users.noreply.github.com>
2024-04-13 19:10:35 +0200

    Chore: Add coordinate system initialization and drawing functions

--- Geo-Math/coordinate_system.py
+++ Geo-Math/coordinate_system.py
@@ -1,53 +1,125 @@
 import curses
 
-def draw(middle_y, coordinates = None):
-    try:
 
-        coordinate_system = curses.newwin(60, 100, 0, middle_y)
+coordinates = {
+    "P": []
+}
+
+indexes = {
+    "X": {
+        
+    },
+
+    "Y": {
+
+    }
+}
+
+def initScreen(x, y):
+    try:
+        coordinate_system = curses.newwin(y, x, 0, 0)
+
+        return coordinate_system
+    except:
+        return False
+    
+
+def draw_coordinate_system(coordinate_system, space_x:int, space_y:int):
+    global middle_x
+    global middle_y
+    try:
+            
+        # Get height and width of window
         height, width = coordinate_system.getmaxyx()
 
         # Calculate the middle position
         middle_x = height // 2
         middle_y = width // 2
 
+        rate_y = 1 / space_y
+        rate_x = 1 / space_x 
+
+
+        # Clear and display debug info
         coordinate_system.clear()
-        coordinate_system.addstr(0, 0, f"{height}|{width}", curses.A_STANDOUT)
+        coordinate_system.addstr(0, 0, f"Window Size: {height}|{width}", curses.A_STANDOUT)
+        coordinate_system.addstr(1, 0, f"Middle: {middle_x}|{middle_y}", curses.A_STANDOUT)
 
-        # Start with the hightest possible Y-Value
-        count = int(round(height /4, 0))
+        # Render the Y-Axis (Without numbers)
+        len_y = 0
+        for i in range(0, height, 1):
+            coordinate_system.addstr(i, middle_y, "|")
+            len_y += 1
+            
 
-        # Render the Y-Axis
-        for i in range(0, height):
-            # Check if index is even
-            if i % 2 == 0:
-                coordinate_system.addstr(i, middle_y, f"{count}")
-
-                # Check if coordinates are at the X Level
-                if i > middle_x:
-                    # If it is under subtract
-                    count -= 1
-                elif i == middle_x:
-                    # Set it to -1
-                    count = -1
-                else:
-                    count -= 1
-            else:
-                if  i == middle_x:
-                    count = -1
-
-                coordinate_system.addstr(i, middle_y, "|")
-
-
-        # Render the X-Axis
-        for i in range(0, width):
-                coordinate_system.addstr(middle_x, i, f"-")
+        # Render negative values (Y-Axis)
+        index = 0
+        for i in range(middle_x, height, space_y):
+            coordinate_system.addstr(i, middle_y, f"# {index}")
+            indexes["Y"][index] = i
+            index -= 1
         
+        # Render positive values (Y-Axis)
+        index = 0
+        for i in range(middle_x, 0, -space_y):
+            coordinate_system.addstr(i, middle_y, f"# {index}")
+            indexes["Y"][index] = i
+            index += 1
+
+
+
+        # Render the X-Axis (without values)
+        len_x = 0
+        for i in range(0, width, 1):
+            coordinate_system.addstr(middle_x, i, "-")
+            len_x += 1
+        
+        # Render positive values (X-Axis)
+        index = 1
+        for i in range(middle_y +space_x, width, space_x):
+            coordinate_system.addstr(middle_x, i, "#")
+            coordinate_system.addstr(middle_x -1, i, f"{index}")
+            indexes["X"][index] = i
+            index += 1
+        
+        # Render negative values (X-Axis)
+        index = -1
+        for i in range(middle_y -space_x, 0, -space_x):
+            coordinate_system.addstr(middle_x, i, "#")
+            coordinate_system.addstr(middle_x -1, i, f"{index}")
+            indexes["X"][index] = i
+            index -= 1
+        
+            
 
         coordinate_system.addstr(middle_x, middle_y, f"0", curses.A_STANDOUT)
-        
-
-        coordinate_system.refresh()
-        coordinate_system.getkey()
+    
+    
     except Exception as e:
         print(e)
         input()
+
+def draw(coordinate_system):
+    for coordinate in coordinates['P']:
+        x_value = coordinate[0]
+        y_value = coordinate[1]
+
+        if x_value == 0:
+            x = middle_y
+        else:
+            x = indexes["X"][x_value]
+        
+        if y_value == 0:
+            y = middle_x
+
+        else:
+            y = indexes["Y"][y_value]
+
+        coordinate_system.addstr(y, x, f"X ({x_value}|{y_value})")
+    coordinate_system.refresh()
+
+
+def new_coordinate(coordinate:list):
+    x_value = coordinate[0]
+    y_value = coordinate[1]
+    coordinates["P"].append([x_value, y_value])
--- Geo-Math/main.py
+++ Geo-Math/main.py
@@ -1,11 +1,39 @@
 from curses import wrapper
 import coordinate_system
+import curses
     
 
 def main(stdscr):
-    height, _ = stdscr.getmaxyx()
+    # Initilise window for coordinate-system
 
-    coordinate_system.draw(middle_y=height // 2)
+    x = 100
+    y = 55
+    window = False
+
+    while not  window:
+        # Try init
+        window = coordinate_system.initScreen(x, y)
+
+        if not window:
+            if x != 20:
+                y -= 1
+                if y == 0:
+                    x -= 1
+                    y = 60
+            else:
+                # Trow a exception
+                Exception("Could not initilise Coordinate-System screen")
+
+    coordinate_system.draw_coordinate_system(window, space_x=4, space_y=2)
+    coordinate_system.new_coordinate([3, 6])
+    coordinate_system.new_coordinate([4, -2])
+    coordinate_system.new_coordinate([-3, -1])
+    coordinate_system.new_coordinate([-2, 5])
+    coordinate_system.draw(window)
+
+    window.refresh()
+    window.getch()
+    curses.endwin()
 
 print("GEO-MATH | Coordinate-System Test | Luis Schuimer 2024")
 input("Please open Geo-Math in full screen mode and press 'Enter' to continue...")
